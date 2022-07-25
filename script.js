d6a8b5102523c9fa56dd97e003be61ff98d2df95
whas5up <chloesow33@gmail.com>
2022-07-25 14:57:21 -0700

    excercises

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 // Exercise 1: 
 // make toggle equal the item with the class 'toggle'
 // and menu equal the item with the class 'menu'
-const toggle = undefined;
-const menu = undefined;
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+
 
 // Exercise 2:
-// makee a function called 'toggleMenu' that 
+// make a function called 'toggleMenu' that 
+function toggleMenu(){
+    console.log('working')
+}
+
 // writes 'working' to the console
 
 // Exercise 3:
+toggle.addEventListener("click", toggleMenu);
 // Add an event listener to the toggle element
 // to run the toggleMenu function
