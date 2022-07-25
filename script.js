67f7fdcc1448c696748bd62808045a868ad246b2
jimboweb <jimboweb@gmail.com>
2022-07-25 15:46:10 -0400

    add exercises  to js comments

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+// Exercise 1: 
 // make toggle equal the item with the class 'toggle'
 // and menu equal the item with the class 'menu'
 const toggle = undefined;
 const menu = undefined;
+
+// Exercise 2:
+// makee a function called 'toggleMenu' that 
+// writes 'working' to the console
+
+// Exercise 3:
+// Add an event listener to the toggle element
+// to run the toggleMenu function
