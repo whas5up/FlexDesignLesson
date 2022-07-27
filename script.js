5d7915fa1e3cabc8f176aa799f086a59af1c2884
whas5up <chloesow33@gmail.com>
2022-07-27 14:55:29 -0700

    toggle menu

--- script.js
+++ script.js
@@ -12,6 +12,15 @@ function toggleMenu(){
 }
 
 // writes 'working' to the console
+function toggleMenu(){
+    if(menu.classList.contains("active")){
+    // the menu is already open, close it
+    menu.classList.remove("active");
+} else {
+    // the menu is closed, open it
+    menu.classList.add("active");
+}
+}
 
 // Exercise 3:
 toggle.addEventListener("click", toggleMenu);
